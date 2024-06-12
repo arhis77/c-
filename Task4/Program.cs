a733044690742215ecfb96b3f7cc1c115204adaf
Alexey Arkhipenko <arhis77@hotmail.com>
2024-06-12 14:12:19 +0300

    add lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int с = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max) {
+	max = b;
+}
+
+if (с > max) {
+	max = с;
+}
+
+if (d > max) {
+	max = d;
+}
+
+if (e > max) {
+	max = e;
+}
+
+Console.WriteLine(max);
